eadf0d116e47c70692379885aa5df77f1e991b70
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-05-15 20:06:53 +0530

    Added skeleton classes to parse the UPnP DIDL output in a more generic way. This will be used in both the kioslave and the Amarok Collection.
    DIDL::Parser - actual parser which emits signals on various elements
    didlobjects is the heirarchy reflecting the DIDL-Lite XML schema

---
+++ didlobjects.h
@@ -0,0 +1,91 @@
+/********************************************************************
+ This file is part of the KDE project.
+
+Copyright (C) 2010 Nikhil Marathe <nsm.nikhil@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
+#ifndef DIDL_OBJECTS_H
+#define DIDL_OBJECTS_H
+
+#include <QUrl>
+
+namespace DIDL {
+
+// container and item form a nice hierarchy,
+// but this is there just to get templates to work fine
+// ah.. the things needed for standards compliance :-)
+// oops, ok id is common
+class SuperItem : public QObject
+{
+  Q_OBJECT
+  public:
+    enum Type {
+        Description,
+        Item,
+        Container
+    };
+
+    SuperItem( Type t, const QString &id ) : m_type(t), m_id(id) {};
+    Type type() const { return m_type; };
+    QString id() const { return m_id; };
+
+  private:
+    Type m_type;
+    QString m_id;
+};
+
+/**
+ * Contains a DIDL 'desc' element.
+ * Called Description because the element is a 'descriptor',
+ * but it contains a description.
+ * A description doesn't care about internal elements and so on.
+ * It stores everything inside as text.
+ */
+class Description : public SuperItem
+{
+  Q_OBJECT
+  public:
+    /**
+     * Create a new Description with the id and the namespace
+     */
+    Description( const QString &id, const QUrl &ns );
+
+    QString description() const { return m_description; };
+    void setDescription( const QString &desc );
+
+    QUrl nameSpace() const { return m_namespace; };
+
+    QString type() const { return m_type; };
+    void setType( const QString &type );
+
+  private:
+    QString m_description;
+    QString m_type;
+    QUrl m_namespace;
+};
+
+inline void Description::setDescription( const QString &desc )
+{
+    m_description = desc;
+}
+
+inline void Description::setType( const QString &type )
+{
+    m_type = type;
+}
+} //~ namespace
+
+#endif
---
+++ didlparser.h
@@ -0,0 +1,105 @@
+/********************************************************************
+ This file is part of the KDE project.
+
+Copyright (C) 2010 Nikhil Marathe <nsm.nikhil@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
+#ifndef DIDL_PARSER_H
+#define DIDL_PARSER_H
+
+#include <QObject>
+#include <QStack>
+
+class QXmlStreamReader;
+
+namespace DIDL {
+
+class Container;
+class Item;
+class Description;
+class SuperItem;
+
+/**
+ * This class implements a parser for the 
+ * DIDL-Lite XML schema. It is meant to be
+ * used with UPnP ContentDirectory service replies.
+ * Required elements are checked for, and an error
+ * occurs in case of absence.
+ * The class emits certain signals when top-level elements
+ * are found. Top-level means immediate children of the DIDL
+ * root element. Their children can be accessed through the
+ * elements themselves.
+ * 
+ * @see DIDL::Container
+ * @see DIDL::Item
+ * @see DIDL::Description
+ * 
+ * @author Nikhil Marathe
+ */
+class Parser : public QObject
+{
+  Q_OBJECT
+  public:
+    Parser();
+    ~Parser();
+
+  public slots:
+    /**
+     * This is NOT a push parser.
+     * @c parse() should only be called once
+     * with the complete XML input until one
+     * off @c done() or @c error() is called.
+     * 
+     * Calling this again will restart the parser.
+     */
+    void parse(const QString &input);
+
+  signals:
+    /**
+     * Emitted in case of an error at any point
+     * The parser will stop parsing after this.
+     */
+    void error(const QString errorString);
+
+    /**
+     * Emitted when parsing is finished.
+     */
+    void done();
+
+    // NOTE should we emit pointers?
+    /**
+     * Emitted when a top-level <item> is completely parsed
+     */
+    void item(Item *);
+
+    /**
+     * Emitted when a top-level <container> is completely parsed.
+     */
+    void container(Container *);
+
+    /**
+     * Emitted for a top-level <desc> element is parsed.
+     */
+    void desc(Description *);
+
+ private:
+    QXmlStreamReader *m_reader;
+    QStack<SuperItem> m_stack;
+};
+
+} //~ namespace
+
+#endif
