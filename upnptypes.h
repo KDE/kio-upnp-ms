4c8a35d49e08dd1201e062bd9e9960b554685f68
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-05-26 11:58:35 +0530

    Added UPnPFieldTypes enumeration to extends UDSEntry::FieldTypes with custom properties useful for Amarok Collection and others

---
+++ upnptypes.h
@@ -0,0 +1,31 @@
+/********************************************************************
+ This file is part of the KDE project.
+
+Copyright (C) 2010 Nikhil Marathe <nsm.nikhil@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
+#ifndef UPNPTYPES_H
+#define UPNPTYPES_H
+
+#include <kio/udsentry.h>
+
+namespace KIO {
+enum UPnPFieldTypes {
+    UPNP_CLASS = UDSEntry::UDS_EXTRA + 1 | UDSEntry::UDS_STRING,
+};
+}
+
+#endif
