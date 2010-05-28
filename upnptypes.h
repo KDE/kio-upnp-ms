57d7b10c483070f3affbf5f7a284d384d018b0e9
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-05-28 11:52:59 +0530

    Added UDSEntry fields for DLNA recommended meta-data

--- upnptypes.h
+++ upnptypes.h
@@ -17,14 +17,71 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 
+/**
+ * @file Provides extra UDSEntry field types for UPnP specific meta-data
+ */
+
 #ifndef UPNPTYPES_H
 #define UPNPTYPES_H
 
 #include <kio/udsentry.h>
 
 namespace KIO {
+
+/**
+* This enumeration defines some custom UDS fields to allow
+* UPnP metadata to be exposed. UPnP provides a number
+* of fields for meta-data which adheres to the recommended
+* properties in the DLNA specification ( August 2009 )
+*
+* Certain fields are shared, such as UPNP_DURATION which applies
+* to audioItems and videoItems.
+* 
+* When a resource is available, size is advertised using the
+* UDS_SIZE value, there is no special UPNP_SIZE.
+*
+* Note: when adding new fields, remember, the restriction is 39
+* fields.
+*/
 enum UPnPFieldTypes {
     UPNP_CLASS = ( UDSEntry::UDS_EXTRA + 1 ) | UDSEntry::UDS_STRING,
+
+    /// object.item.audioItem
+
+    /// The creator is usually the Artist
+    UPNP_CREATOR = ( UDSEntry::UDS_EXTRA + 2 ) | UDSEntry::UDS_STRING,
+    /// The Album to which the item belongs
+    UPNP_ALBUM = ( UDSEntry::UDS_EXTRA + 3 ) | UDSEntry::UDS_STRING,
+    /// The genre
+    UPNP_GENRE = ( UDSEntry::UDS_EXTRA + 4 ) | UDSEntry::UDS_STRING,
+    /// Duration of the content in "HH:MM:SS"
+    UPNP_DURATION = (UDSEntry::UDS_EXTRA + 5 ) | UDSEntry::UDS_STRING,
+
+    /// object.item.imageItem
+
+    // NOTE: for date, should we convert if the device isn't formatting properly?
+    // TODO: should we use the UDS_TIME format modeled by long long
+    // instead and handle the conversion?
+
+    /// the date the image was taken. This may not be the same
+    /// as the access time or file creation time and so has a
+    /// separate field.
+    /// Date is in ISO format, see the UPnP ContentDirectory service
+    /// specification for details.
+    UPNP_DATE = ( UDSEntry::UDS_EXTRA + 6 ) | UDSEntry::UDS_STRING,
+    /// Image resolution, "[0-9]+x[0-9]+" in pixels
+    UPNP_IMAGE_RESOLUTION = ( UDSEntry::UDS_EXTRA + 7 ) | UDSEntry::UDS_STRING,
+
+    // object.item.videoItem
+    // none unique
+
+    /// object.container.album.musicAlbum
+    UPNP_ALBUM_CHILDCOUNT = ( UDSEntry::UDS_EXTRA + 8 ) | UDSEntry::UDS_NUMBER,
+
+    /// object.item.videoItem.videoBroadcast, object.item.audioItem.audioBroadcast
+    UPNP_CHANNEL_NAME = ( UDSEntry::UDS_EXTRA + 9 ) | UDSEntry::UDS_STRING,
+    UPNP_CHANNEL_NUMBER = ( UDSEntry::UDS_EXTRA + 10 ) | UDSEntry::UDS_NUMBER
+
 };
 }
 
